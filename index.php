c3da2dd4cbc43a93e5b6a72706547920a7b1635f
neca230 <jannethfot@gmail.com>
2025-03-05 19:02:13 +0100

    Z

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>VIAJE USA</title>
-    <link rel='icon' type="image/png" href="usa.png">
+    <link rel="icon" type="image/png" href="usa.png">
 </head>
 <body>
 
@@ -14,17 +14,19 @@
                 USA Trip
                 <i class="fa-brands fa-php fa-xl"></i>
             </a>
+            <button class="navbar-toggler" data-bs-toggle= "collapse" data-bs-targer="menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
 </nav>
     <div class="container mt-3 col-md-3">
         <h2 class="text-center">Registro de Gastos</h2>
-        <form action="procesar.php" method="POST">
+        <form action="index.php" method="GET">
             <label class="form-label" for="nombre">Gastos:</label>
             <input type="hidden" id="accion" name="accion" value="agregar">
             <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Gastos" required><br>
             <label for="telefono">Monto:</label>
-
-            <input type="email" class="form-control" id="correo" name="correo" placeholder="Monto" required><br>
+            <input type="moneda" class="form-control" id="correo" name="correo" placeholder="Monto" required><br>
             <button type="submit" class="btn btn-primary">Guardar</button>
         </form>
     </div>
@@ -34,64 +36,21 @@
                     <thead class="table-dark">
                         <tr>
                             <th>ID</th>
-                            <th>NOMBRE</th>
-                            <th>TELÉFONO</th>
-                            <th>CORREO</th>
-                            <th>ACCIONES</th>
+                            <th>GASTOS</th>
+                            <th>BASE</th>
+                            <th>IVA (21%)</th>
+                            <th>MONTO</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td class="wrap"><?php echo $counter; ?></td>
-                            <td class="wrap"><?php echo $row['nombre']; ?></td>
-                            <td class="wrap"><?php echo $row['telefono']; ?></td>
-                            <td class="wrap"><?php echo $row['correo']; ?></td>
-                            <td class="wrap d-inline-flex gap-1 botones justify-content-center">
-                                <a href="editar.php?id=<?php echo $row['id']; ?>" class="btn btn-sm btn-primary">Editar <i class='fa-solid fa-user-pen'></i></a>
+            <tr>
+                   <td class="wrap d-inline-flex gap-1 botones justify-content-center">
+            <?php
 
-                                <form action="procesar.php" method="POST">
-                                    <input type="hidden" id="accion" name="accion" value="borrar">
-                                    <input type="hidden" id="id" name="id" value="<?php echo $row['id']; ?>">
-                                    <button class="btn btn-sm btn-danger">Eliminar <i class='fa-solid fa-user-xmark'></i></button>
-                                </form>
-                            </td>
-                        </tr>
-    <?php
-                }
     ?>
                     </tbody>
-        </table>
-    </div>
-    <?php
-        }
-        else {
-    ?>
-
-            <div class="container col-md-3 mt-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">No hay Datos</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">Bo se han enc...</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container mt-3">
-                <img src="banner.jpg" class="img-fluid" alt="No Data">
+                </table>
             </div>
     <?php
-        }
-        $conn->close();
     ?>
-
-    <!-- <footer class="text-center text-white mt-5">
-        <div class="bg-primary p-3">
-            Copyright - Carlos Ochoa 2025 -
-            <a class="text-white" href="#">https://ochoa.es</a>
-        </div>
-    </footer> -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
