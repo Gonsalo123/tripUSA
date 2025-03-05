56b25aba14d8d1b31b287f94a733e555a55e8914
neca230 <jannethfot@gmail.com>
2025-03-05 21:02:51 +0100

    z

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include 'conn.php';
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -25,31 +28,66 @@
             <label class="form-label" for="nombre">Gastos:</label>
             <input type="hidden" id="accion" name="accion" value="agregar">
             <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Gastos" required><br>
-            <label for="telefono">Monto:</label>
-            <input type="moneda" class="form-control" id="correo" name="correo" placeholder="Monto" required><br>
+            <label for="moneda">Monto:</label>
+            <input type="moneda" class="form-control" id="monto" name="monto" placeholder="Monto" required><br>
             <button type="submit" class="btn btn-primary">Guardar</button>
         </form>
     </div>
 
-    <div class="container mt-3 table-responsive">
-        <table class="table table-hover text-center">
+ <?php
+
+        $query = "SELECT * FROM contactos";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+    ?>
+            <div class="container mt-3 table-responsive">
+                <table class="table table-hover text-center">
                     <thead class="table-dark">
                         <tr>
-                            <th>ID</th>
+                            <th>#</th>
                             <th>GASTOS</th>
                             <th>BASE</th>
                             <th>IVA (21%)</th>
                             <th>MONTO</th>
                         </tr>
                     </thead>
-            <tr>
-            <?php
+                    <tbody>
+    <?php
+                $counter = '';
+                while ($row = $result->fetch_assoc()) {
+                    $counter++;
+    ?>
+                        <tr>
+                            <td class="wrap"><?php echo $counter; ?></td>
+                            <td class="wrap"><?php echo $row['gasto']; ?></td>
+                            <td class="wrap"><?php echo $row['monto']; ?></td>
+                            <td class="wrap d-inline-flex gap-1 botones justify-content-center"></td>
 
+
+
+                        </tr>
+    <?php
+                }
     ?>
                     </tbody>
                 </table>
             </div>
     <?php
+        }
+        else {
     ?>
+
+
+<!--
+            <div class="container mt-3">
+                <img src="banner.jpg" class="img-fluid" alt="No Data">
+            </div> -->
+    <?php
+        }
+        $conn->close();
+    ?>
+
+
 </body>
 </html>
